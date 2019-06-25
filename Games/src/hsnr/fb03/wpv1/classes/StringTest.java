06a446571aa4aea359369abc084ae3aecac55f09
Gladis Douabalet Maika <douabalet@incas.de>
2019-06-25 13:33:24 +0200

    Problemdarstellung

---
+++ Games/src/hsnr/fb03/wpv1/classes/StringTest.java
@@ -0,0 +1,22 @@
+package hsnr.fb03.wpv1.classes;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		String s = new String("Hallo world");
+		System.out.println(s);
+	}
+
+}
+
+class String{
+	private final java.lang.String s;
+	
+	public String(java.lang.String s){
+		this.s = s;
+	}
+	
+	public java.lang.String toString(){
+		return s;
+	}
+}
---
+++ Games/src/hsnr/fb03/wpv1/principal/B10U5.java
@@ -0,0 +1,10 @@
+package hsnr.fb03.wpv1.principal;
+
+public class B10U5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

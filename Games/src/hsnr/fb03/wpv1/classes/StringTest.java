a6f6116b9355b1d8cfa17f8d8e650ff9ae573161
Gladis Douabalet Maika <douabalet@incas.de>
2019-06-25 13:44:09 +0200

    Verbesserung

--- Games/src/hsnr/fb03/wpv1/classes/StringTest.java
+++ Games/src/hsnr/fb03/wpv1/classes/StringTest.java
@@ -2,7 +2,7 @@ package hsnr.fb03.wpv1.classes;
 
 public class StringTest {
 
-	public static void main(String[] args) {
+	public void start() {
 		hsnr.fb03.wpv1.classes.objects.String s = new hsnr.fb03.wpv1.classes.objects.String("Hallo world");
 		System.out.println(s);
 	}
